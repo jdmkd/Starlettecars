d16174bbdb787bc03cd37f79d428a44871f282aa
jdmkd <jdmkd49@gmail.com>
2025-07-10 08:30:25 -0700

    project: Update root URLs

--- rentalproject/urls.py
+++ rentalproject/urls.py
@@ -27,10 +27,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("rentalapp.urls")),
     path("rental_business/", include("rental_business.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
+    
 
 
     # re_path('admin-interface/', include('admin_interface.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")),)
